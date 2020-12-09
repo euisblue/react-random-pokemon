0eff336772dbb5428d66f81e620b1fbb84753678
.Blue <yuueu@pm.me>
2020-12-09 22:14:52 +0900

    Pokemon random generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React from "react";
+import axios from "axios";
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      name: '',
+      imgSrc: '',
+    }
+  }
+  async handleClick() {
+    const max = 895;
+    const min = 1;
+
+    let index = Math.floor(Math.random() * max + min);
+
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`);
+    console.log(response.data.sprites)
+    let name = response.data.forms[0].name;
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+
+    let src = response.data.sprites.other["official-artwork"].front_default;
+    if (src === '' || src === undefined || src === null) {
+      src = response.data.sprites.front_default;
+    }
+    this.setState({
+      name: name,
+      imgSrc: src
+    })
+    console.log(this.state.imgSrc);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>{this.state.name}</header>
+        <img src={this.state.imgSrc} alt=" " />
+        <button onClick={() => { this.handleClick() }}>Generate</button>
+      </div>
+    );
+  }
+}
